3b4b8b26b98345065b19a310deca4044ae102d1d
Senipah <n.morgan86@googlemail.com>
2020-08-01 20:46:48 +0100

    fix res

--- scripts/minify.js
+++ scripts/minify.js
@@ -19,15 +19,15 @@ fs.readFile(style, function (err, data) {
         mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
         mergeMedia: true, // controls `@media` merging; defaults to true
         mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
-        mergeSemantically: false, // controls semantic merging; defaults to false
+        mergeSemantically: true, // controls semantic merging; defaults to false
         overrideProperties: true, // controls property overriding based on understandability; defaults to true
         removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
         reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
         removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
         removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
         removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
-        removeUnusedAtRules: false, // controls unused at rule removing; defaults to false (available since 4.1.0)
-        restructureRules: false, // controls rule restructuring; defaults to false
+        removeUnusedAtRules: true, // controls unused at rule removing; defaults to false (available since 4.1.0)
+        restructureRules: true, // controls rule restructuring; defaults to false
         skipProperties: [], // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
       },
     },
